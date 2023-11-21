b6cc8719eff461c279c11527e2b202a4766b7536
albertoribeiro <alberto.r.flavio@gmail.com>
2023-11-20 20:18:17 -0500

    chore: fix package-json

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,10 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}'
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-export default config
